3ef52bbcafd5a80d365a36958546d29807858516
AronDas <arondas.sg@gmail.com>
2026-02-03 18:38:46 -0400

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+
+number = 67 #integer - intl
+number1 = 41.79 #floating datatype - float
+name = "Aron Das" #string - str
+result = True #boolean - bool
+
+print ("data type of number is:", type(number))
+print ("data type of number1 is:", type(number1))
+print ("data type of name is:", type(name))
+print ("data type of result is:", type(result))
+
+number2 = float(number)
+print(number2,type(number2))
+
+number3 = int(number1)
+print(number3,type(number3))
